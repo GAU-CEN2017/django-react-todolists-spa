ddd1803f1198add9dc0ab8f9b633f452ada249a4
Olga Pinheiro <olgapinheirogarcia@gmail.com>
2018-07-18 17:45:43 -0300

    Fixed bug in API

--- todotoday/todoapp/api.py
+++ todotoday/todoapp/api.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from knox.models import AuthToken
 
 from .models import TodoList, Task
-from .serializers import (ListSerializer, TaskSerializer,
+from .serializers import (ListSerializer, TaskSerializer, CreateUserSerializer,
                             UserSerializer, LoginUserSerializer)
 
 
