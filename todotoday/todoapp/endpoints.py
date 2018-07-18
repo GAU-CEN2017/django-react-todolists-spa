e66371a42f0b5c3974361125673c0e571b4d0ff6
Olga Pinheiro <olgapinheirogarcia@gmail.com>
2018-07-18 17:31:10 -0300

    Added User authentication API, endpoints and serializers

--- todotoday/todoapp/endpoints.py
+++ todotoday/todoapp/endpoints.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from .api import ListViewSet, TaskViewSet
+from .api import ListViewSet, TaskViewSet, RegistrationAPI, LoginAPI, UserAPI
 
 router = routers.DefaultRouter()
 router.register('lists', ListViewSet)
@@ -9,4 +9,7 @@ router.register('tasks', TaskViewSet)
 
 urlpatterns = [
     url("^", include(router.urls)),
+    url("^auth/register/$", RegistrationAPI.as_view()),
+    url("^auth/login/$", LoginAPI.as_view()),
+    url("^auth/user/$", UserAPI.as_view()),
 ]
--- todotoday/todoapp/serializers.py
+++ todotoday/todoapp/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 from .models import TodoList
@@ -31,3 +32,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username')
+
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Unable to log in with provided credentials.")
